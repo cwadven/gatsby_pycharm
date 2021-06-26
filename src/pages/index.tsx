fa1db49efb342411c9cad8c82b2b653309df1009
이창우 <cwadven@naver.com>
2021-06-27 00:43:12 +0900

    feat: EmotionJS
    - 적용 방법은 Notion에 작성

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,23 @@
 import React, { FunctionComponent } from 'react';
-import Text from 'components/Text';
+import styles from '@emotion/styled';
+import GlobalStyle from 'components/Common/GlobalStyle';
+import Introduction from 'components/Main/Introduction';
+import Footer from 'components/Main/Footer';
+
+const Container = styles.div`
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+`;
 
 const IndexPage: FunctionComponent = function () {
-    return <Text text="Home" />;
+    return (
+        <Container>
+            <GlobalStyle />
+            <Introduction />
+            <Footer />
+        </Container>
+    );
 };
 
 export default IndexPage;
