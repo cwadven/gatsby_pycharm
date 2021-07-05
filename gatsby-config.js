5ea0bb11567acc642aad0598ae451f0231953a05
이창우 <cwadven@naver.com>
2021-07-05 21:50:19 +0900

    feat: meta tag 적용
    - meta tag 적용

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,10 @@
 module.exports = {
+    siteMetadata: {
+        title: `주니어 개발자의 개발 블로그`,
+        description: `주니어 개발자로서의 저를 표현한 블로그입니다.`,
+        author: `Lee`,
+        siteUrl: 'https://my-website-link.com', // 배포 후 변경 예정
+    },
     flags: {
         DEV_SSR: false,
     },
